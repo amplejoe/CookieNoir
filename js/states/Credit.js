f0314fe5f70e5e99a7cabc12f866c9471870f141
GuentherK <gkoessle@gmail.com>
2017-04-30 00:35:45 +0200

    Credits überarbeitet

--- js/states/Credit.js
+++ js/states/Credit.js
@@ -12,19 +12,32 @@ CookieNoir.Credit.prototype =
 
   },
 
-  addCredit: function(task, author) {
-    let authorStyle = { font: '40pt TheMinion', fill: 'white', align: 'center', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
-    let taskStyle = { font: '30pt TheMinion', fill: 'white', align: 'center', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
-    let authorText = this.add.text(this.world.centerX, 900, author, authorStyle);
-    let taskText = this.add.text(this.world.centerX, 950, task, taskStyle);
-    authorText.anchor.setTo(0.5);
-    authorText.stroke = "rgba(0,0,0,0)";
-    authorText.strokeThickness = 4;
+  addCredit: function(task, authors) {
+    let authorStartX = 950;
+    let authorStartY = -400;
+    let authorStyle = { font: '30pt TheMinion', fill: 'white', align: 'center', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
+    let taskStyle = { font: '40pt TheMinion', fill: 'white', align: 'center', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
+    let taskText = this.add.text(this.world.centerX, 900, task, taskStyle);
+
     taskText.anchor.setTo(0.5);
     taskText.stroke = "rgba(0,0,0,0)";
     taskText.strokeThickness = 4;
-    this.add.tween(authorText).to( { y: -300 }, 20000, Phaser.Easing.Cubic.Out, true, this.creditCount * 10000);
-    this.add.tween(taskText).to( { y: -200 }, 20000, Phaser.Easing.Cubic.Out, true, this.creditCount * 10000);
+    this.add.tween(taskText).to( { y: -500 }, 20000, Phaser.Easing.Cubic.Out, true, this.creditCount * 2500);
+
+    if (authors !== undefined) {
+      authors.forEach((element) => {
+        let authorText = this.add.text(this.world.centerX, authorStartX, element, authorStyle);
+        authorText.anchor.setTo(0.5);
+        authorText.stroke = "rgba(0,0,0,0)";
+        authorText.strokeThickness = 4;
+        this.add.tween(authorText).to({
+          y: authorStartY
+        }, 20000, Phaser.Easing.Cubic.Out, true, this.creditCount * 2500);
+        authorStartX += 100;
+        authorStartY += 100;
+      });
+    }
+
     this.creditCount ++;
   },
 
@@ -56,25 +69,25 @@ CookieNoir.Credit.prototype =
   create: function () {
     this.stage.disableVisibilityChange = true;
     let bg = this.add.sprite(0, 0, 'gameover-bg');
-    this.addCredit('Music', '??');
+    this.addCredit('Music', ['??']);
 
-    this.addCredit('Game-Design & Story', 'Daniela Brus');
-    this.addCredit('Game-Design & Story', 'Felix Schniz');
+    this.addCredit('Map-Design & Story', ['Daniela Brus', 'Felix Schniz']);
 
-    this.addCredit('Artist', 'Natascha Rauscher');
+    this.addCredit('Artist & Design', ['Natascha Rauscher']);
 
-    this.addCredit('Dev', 'Andres Leibetseder');
-    this.addCredit('Dev', 'Tarek Abdel Aziz');
-    this.addCredit('Dev', 'Günther Kössler');
+    this.addCredit('Dev', ['Andres Leibetseder', 'Tarek Abdel Aziz', 'Günther Kössler']);
 
-    this.addCredit('Phaser.io', 'Powered By');
-    this.addCredit('GameJam17', 'Powered By Anexia');
+    this.addCredit('Powered By', ['Phaser.io']);
+    this.addCredit('GameJam17', ['Powered By Anexia']);
 
-    this.addCredit('for playing', 'Thank you');
+    this.addCredit('Thank you for playing', ['']);
+    this.addCredit("It's not a bug it a feature ;-)", ['']);
+
+
     this.addMenuOption('<- Back', function (e) {
       this.state.start("Title");
     });
-    this.add.tween(bg).to({alpha: 0}, 20000, Phaser.Easing.Cubic.Out, true, 40000);
+    this.add.tween(bg).to({alpha: 0}, 20000, Phaser.Easing.Cubic.Out, true, 10000);
   }
 
 };
